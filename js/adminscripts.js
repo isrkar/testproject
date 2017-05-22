e9feb7587baa81074a079e17628a317fe2a39a03
isrkar <isr.kar@gmail.com>
2017-05-22 18:39:45 +0300

    admin frontend pagination complete

--- js/adminscripts.js
+++ js/adminscripts.js
@@ -1,4 +1,4 @@
-function generate_table(table_data) {
+function generate_table(object, table_data) {
     var table = $('<table></table>').addClass('table table-responsive table-hover');
     var thead = $('<thead></thead>');
     thead = generate_table_header(thead, ['id', 'email']);
@@ -8,10 +8,10 @@ function generate_table(table_data) {
     tbody = generate_table_rows(tbody, table_data);
     table.append(tbody);
 
-    $('#table').append(table);
+    object.append(table);
 }
 
-function generate_table_header(parent, data) {
+function generate_table_header(object, data) {
     row = $('<tr></tr>');
 
     data.forEach(function (table_col, i, cols) {
@@ -19,11 +19,11 @@ function generate_table_header(parent, data) {
         row.append(col);
     });
     row.append('<td></td>');
-    parent.append(row);
-    return parent;
+    object.append(row);
+    return object;
 }
 
-function generate_table_rows(parent, data) {
+function generate_table_rows(object, data) {
     var i;
     for (i = 0; i < data.length; i++) {
         var table_row = data[i];
@@ -35,24 +35,66 @@ function generate_table_rows(parent, data) {
             col.text(table_col);
             row.append(col);
         });
-        parent.append(row);
+        object.append(row);
     }
-    return parent;
+    return object;
 }
+
+function generate_pagination(object, page, total) {
+    var ul = $('<ul></ul>').addClass('pagination');
+    var all_pages = Math.ceil(total / 5);
+
+    for (i = 1; i <= all_pages; i++) {
+        var li = $('<li><a href="#" class="pgbt" data-page="'+i+'">' + i + '</a></li>');
+        if (i == page) li.addClass('active');
+        ul.append(li);
+    }
+
+    object.append(ul);
+}
+
+var total_pages;
+
 
 $(document).ready(function () {
 
 
-    $('#table').empty();
 
     $.ajax({
         type: "GET",
         url: "api.php",
         dataType: 'json',
-        data: "&req=getPage"
+        data: "&req=getCount"
     }).done(function (data) {
-        generate_table(data['data']);
+        $('#table').empty();
+        total_pages = data['count'];
+        $('#total').append(total_pages);
+
+        $.ajax({
+            type: "GET",
+            url: "api.php",
+            dataType: 'json',
+            data: "&req=getPage"
+        }).done(function (data) {
+            generate_table($('#table'), data['data']);
+            generate_pagination($('#table'), data['page'], total_pages);
+        });
     });
 
+
+    $(document).on('click', '.pgbt', function () {
+        console.log(this);
+        var btn=$(this);
+        $.ajax({
+            type: "GET",
+            url: "api.php",
+            dataType: 'json',
+            data: "&req=getPage&pageNum="+btn.data('page')
+        }).done(function (data) {
+            $('#table').empty();
+            generate_table($('#table'), data['data']);
+            generate_pagination($('#table'), data['page'], total_pages);
+        });
+    });
 
 });
